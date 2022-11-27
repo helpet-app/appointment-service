31f18f3f9ac67d5b307a4a1abbbceb321c4ab130
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-11-27 17:37:02 +0700

    Add exception handler

---
+++ src/main/java/com/helpet/appointmentservice/web/util/BaseExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.helpet.appointmentservice.web.util;
+
+
+import com.helpet.web.handler.LocalizedExceptionHandler;
+import com.helpet.web.util.Localizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BaseExceptionHandler extends LocalizedExceptionHandler {
+    @Autowired
+    public BaseExceptionHandler(Localizer localizer) {
+        super(localizer);
+    }
+}
