2ed41f0f26bf8091233f432774257c37032e0c78
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-11-30 19:23:14 +0700

    Delete unnecessary error enums

--- src/main/java/com/helpet/appointmentservice/service/error/FailedToGetFreeTimeSlotsLocalizedError.java
+++
@@ -1,7 +0,0 @@
-package com.helpet.appointmentservice.service.error;
-
-import com.helpet.exception.util.DefaultEnumLocalizedError;
-
-public enum FailedToGetFreeTimeSlotsLocalizedError implements DefaultEnumLocalizedError {
-    VET_DOES_NOT_EXIST
-}
--- src/main/java/com/helpet/appointmentservice/service/error/FailedToGetScheduleLocalizedError.java
+++
@@ -1,7 +0,0 @@
-package com.helpet.appointmentservice.service.error;
-
-import com.helpet.exception.util.DefaultEnumLocalizedError;
-
-public enum FailedToGetScheduleLocalizedError implements DefaultEnumLocalizedError {
-    VET_DOES_NOT_EXIST
-}
