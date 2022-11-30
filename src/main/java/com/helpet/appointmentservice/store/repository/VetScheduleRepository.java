3e28eda5c990dee821e320fa87aa108cf472af20
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-11-30 19:32:47 +0700

    Change VetSchedule to VetScheduleSlot

--- src/main/java/com/helpet/appointmentservice/store/repository/VetScheduleRepository.java
+++ src/main/java/com/helpet/appointmentservice/store/repository/VetScheduleRepository.java
@@ -1,6 +1,6 @@
 package com.helpet.appointmentservice.store.repository;
 
-import com.helpet.appointmentservice.store.model.VetSchedule;
+import com.helpet.appointmentservice.store.model.VetScheduleSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +8,6 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface VetScheduleRepository extends JpaRepository<VetSchedule, UUID> {
-    List<VetSchedule> findAllByVetId(UUID vetId);
+public interface VetScheduleRepository extends JpaRepository<VetScheduleSlot, UUID> {
+    List<VetScheduleSlot> findAllByVetId(UUID vetId);
 }
